1f1fcc816265c6fc21b870119077a7897e0b3780
chou0827 <35863674+chou0827@users.noreply.github.com>
2018-02-24 15:00:39 -0800

    Update water-arrays.js

--- water-arrays.js
+++ water-arrays.js
@@ -6,13 +6,13 @@ function setup() {
   createCanvas(400, 400);
   colorMode(HSB)
 
-	for (var index = 0; index < 10; index = index + 1) {
+	for (var index = 0; index < 100; index = index + 1) {
 		drops[index] = {
 			x : 230,
 			y : 220,
 			dspeed : random(1, 5),
-			dd : random(10, 30)
-			
+			ddia : random(10, 30),
+			c : color (240, random(360), random(360))
 }
 	}}
 
@@ -23,17 +23,15 @@ function draw() {
 
 	for (var index = 0; index < 10; index = index + 1) {
 	var drop = drops[index]
-	
 
-  rect(0, 100, drop.x, 20);
-  
+  fill(360)
+	rect(0, 100, drop.x, 20);
 
-  ellipse(drop.x, drop.y, drop.dd);
-  
+	fill(drop.c);
+  ellipse(drop.x, drop.y, drop.ddia);
 
   drop.y = drop.y + drop.dspeed
-  
- 
+
   if (drop.y > height*random(1,3)) {
     // reset
     drop.y = 120;
